a929c0cdea064195892dd7f9a4250a0cfdcad154
zumorica <zddm@outlook.es>
2019-11-03 15:14:31 +0100

    Add hello world app

---
+++ MysteryOnline/App.xaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace MysteryOnline
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+   }
+}
